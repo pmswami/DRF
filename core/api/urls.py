359c1f606b3804f218a8ee4f362dc58544033719
swamfire <usern5949@gmail.com>
2024-03-05 10:20:50 +0530

    Added @api_view methods

---
+++ core/api/urls.py
@@ -0,0 +1,8 @@
+from home.views import index
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path("index/", index)
+]
--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(["GET", "POST", "PUT", "PATCH"])
+def index(request):
+    courses = {
+            "course_name": "Python",
+            "learn": ["Django", "Tornado", "Python", "Flask", "FastAPI"],
+            "course_provider": "Scaler"
+        }
+    if request.method == "GET":
+        print("You have hit get method")
+    elif request.method == "POST":
+        print("You have hit get method")
+    elif request.method == "PUT":
+        print("You have hit put method")
+    elif request.method == "PATCH":
+        print("You have hit patch method")
+    return Response(courses)
