26a2f291de7d305993d0220d60604d8b389523eb
swamfire <usern5949@gmail.com>
2024-03-05 11:40:18 +0530

    Added serializer put, patch,delete methods

--- core/home/views.py
+++ core/home/views.py
@@ -27,17 +27,41 @@ def index(request):
         print("You have hit patch method")
     return Response(courses)
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "POST", "PUT", "PATCH", "DELETE"])
 def person(request):
     #check request method
     if request.method == "GET":
         objs = Person.objects.all()
         serializer = PeopleSerializer(objs, many=True)
         return Response(serializer.data)
-    else:
+
+    elif request.method=="POST":
         data = request.data
         serializer = PeopleSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+    elif request.method == "PUT":
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    elif request.method == "PATCH":
+        data = request.data
+        obj = Person.objects.get(id = data["id"])
+        serializer = PeopleSerializer(obj, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        data = request.data
+        obj = Person.objects.get(id = data["id"])
+        obj.delete()
+        return Response({"message":"person deleted"})
