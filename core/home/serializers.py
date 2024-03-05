159512946097c56046b410c6bbcf514242acd8ea
swamfire <usern5949@gmail.com>
2024-03-05 11:54:47 +0530

    Added input data validation for serializer

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -8,3 +8,18 @@ class PeopleSerializer(serializers.ModelSerializer):
         fields = "__all__" #include all fields
         # fields = ["name"]  #include specific fields
         # exclude = ["name"] #used to exclude some of the fields
+
+    #validate single input
+    # def validate_age(self, data):
+    #     if(data["age"]<18):
+    #         raise serializers.ValidationError("Age should be greater than 18")
+    #     return data
+
+    #validate all inputs
+    def validate(self,data):
+        special_characters = "!@#$%^&*()_-=+<>?/"
+        if any(c in special_characters for c in data["name"]):
+            raise serializers.ValidationError("Name cannot contain special characters.")
+        if(data["age"]<18):
+            raise serializers.ValidationError("Age should be greater than 18")
+        return data
